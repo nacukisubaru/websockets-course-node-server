c998d804c4cc46bf84f7682666126096c9fba02d
nacukisubaru <alexitsheff@gmail.com>
2022-05-22 20:44:30 +0300

    инициализация вебсокета

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const WSServer = require('express-ws')(app);
 
 const PORT = process.env.PORT || 5000;
 
+app.ws('/', (ws, req) => {
+    console.log(`ПОДКЛЮЧЕНИЕ УСТАНОВЛЕНО`);
+})
+
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
