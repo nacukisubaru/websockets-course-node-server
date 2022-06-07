57bc0c82b37ed26bd6cbd50afc26446923960ea0
nacukisubaru <alexitsheff@gmail.com>
2022-06-07 15:02:08 +0300

    сохранение приходящих файлов на сервере

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const WSServer = require('express-ws')(app);
 const aWss = WSServer.getWss();
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const fs = require('fs');
+
+app.use(cors());
+app.use(express.json());
 
 app.ws('/', (ws, req) => {
     console.log(`ПОДКЛЮЧЕНИЕ УСТАНОВЛЕНО`);
@@ -19,8 +24,35 @@ app.ws('/', (ws, req) => {
                 broadcastConnection(ws, msg);
                 break;
         }
-        console.log(msg);
+        //console.log(msg);
     });
+});
+
+//слушатель пост запросов
+app.post('/image', (req, res)=> {
+    try {
+        const data = req.body.img.replace('data:image/png;base64,', '');
+        //сохранение файла
+        fs.writeFileSync(__dirname+'/files/'+`${req.query.id}.jpg`, data, 'base64');
+        return res.status(200).json({message:'успех'});
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('error');
+    }
+});
+
+//слушатель гет запросов
+app.get('/image', (req, res)=>{
+    try {
+        //чтение файла
+        const file = fs.readFileSync(__dirname+'/files/'+`${req.query.id}.jpg`);
+        const data = 'data:image/png;base64,' + file.toString('base64');
+        //возрат файла клиенту
+        res.json(data);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json('error');
+    }
 });
 
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
